e57eddd34a050a36b685cdd472a11264ba0d8fcd
hsuhau <hsuhau@foxmail.com>
2019-12-29 17:30:19 +0800

    “提交代码”

--- src/main/java/com/hsuhau/easy/DefangIPAddr.java
+++ src/main/java/com/hsuhau/easy/DefangIPAddr.java
@@ -6,4 +6,7 @@ public class DefangIPAddr {
     public String defangIPAddr(String address) {
         return address.replace(".", "[.]");
     }
+    public static void main(String[] args) {
+        System.out.println("1");
+    }
 }
