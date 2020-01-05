2bf780a426190358c124eb0e1efe39061ce6dd73
hsuhau <hsuhau@foxmail.com>
2020-01-05 21:45:53 +0800

    两数相加 更新官方题解

--- src/main/java/com/hsuhau/medium/AddTwoNumbers.java
+++ src/main/java/com/hsuhau/medium/AddTwoNumbers.java
@@ -2,9 +2,6 @@ package com.hsuhau.medium;
 
 import com.hsuhau.common.ListNode;
 
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * 2. 两数相加
  *
@@ -27,46 +24,87 @@ import java.util.List;
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
 class AddTwoNumbers {
-    // todo
-
+    /**
+     * 方法：初等数学
+     * 思路
+     *
+     * 我们使用变量来跟踪进位，并从包含最低有效位的表头开始模拟逐位相加的过程。
+     *
+     *
+     *
+     * 图1，对两数相加方法的可视化: 342 + 465 = 807 342+465=807，每个结点都包含一个数字，并且数字按位逆序存储。
+     *
+     * 算法
+     *
+     * 就像你在纸上计算两个数字的和那样，我们首先从最低有效位也就是列表 l1l1 和 l2l2 的表头开始相加。由于每位数字都应当处于 0 \ldots 90…9 的范围内，我们计算两个数字的和时可能会出现 “溢出”。例如，5 + 7 = 125+7=12。在这种情况下，我们会将当前位的数值设置为 22，并将进位 carry = 1carry=1 带入下一次迭代。进位 carrycarry 必定是 00 或 11，这是因为两个数字相加（考虑到进位）可能出现的最大和为 9 + 9 + 1 = 199+9+1=19。
+     *
+     * 伪代码如下：
+     *
+     * 将当前结点初始化为返回列表的哑结点。
+     * 将进位 carrycarry 初始化为 00。
+     * 将 pp 和 qq 分别初始化为列表 l1l1 和 l2l2 的头部。
+     * 遍历列表 l1l1 和 l2l2 直至到达它们的尾端。
+     * 将 xx 设为结点 pp 的值。如果 pp 已经到达 l1l1 的末尾，则将其值设置为 00。
+     * 将 yy 设为结点 qq 的值。如果 qq 已经到达 l2l2 的末尾，则将其值设置为 00。
+     * 设定 sum = x + y + carrysum=x+y+carry。
+     * 更新进位的值，carry = sum / 10carry=sum/10。
+     * 创建一个数值为 (sum \bmod 10)(summod10) 的新结点，并将其设置为当前结点的下一个结点，然后将当前结点前进到下一个结点。
+     * 同时，将 pp 和 qq 前进到下一个结点。
+     * 检查 carry = 1carry=1 是否成立，如果成立，则向返回列表追加一个含有数字 11 的新结点。
+     * 返回哑结点的下一个结点。
+     * 请注意，我们使用哑结点来简化代码。如果没有哑结点，则必须编写额外的条件语句来初始化表头的值。
+     *
+     * 请特别注意以下情况：
+     *
+     * 测试用例	说明
+     * l1=[0,1]l1=[0,1]，l2=[0,1,2]l2=[0,1,2]	当一个列表比另一个列表长时
+     * l1=[]l1=[]，l2=[0,1]l2=[0,1]	当一个列表为空时，即出现空列表
+     * l1=[9,9]l1=[9,9]，l2=[1]l2=[1]	求和运算最后可能出现额外的进位，这一点很容易被遗忘
+     *
+     * 复杂度分析
+     *
+     * 时间复杂度：O(\max(m, n))O(max(m,n))，假设 mm 和 nn 分别表示 l1l1 和 l2l2 的长度，上面的算法最多重复 \max(m, n)max(m,n) 次。
+     *
+     * 空间复杂度：O(\max(m, n))O(max(m,n))， 新列表的长度最多为 \max(m,n) + 1max(m,n)+1。
+     *
+     * 拓展
+     *
+     * 如果链表中的数字不是按逆序存储的呢？例如：
+     *
+     * (3 \to 4 \to 2) + (4 \to 6 \to 5) = 8 \to 0 \to 7
+     * (3→4→2)+(4→6→5)=8→0→7
+     *
+     * 作者：LeetCode
+     * 链接：https://leetcode-cn.com/problems/add-two-numbers/solution/liang-shu-xiang-jia-by-leetcode/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     *
+     * @param l1
+     * @param l2
+     * @return
+     */
     public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        if (l1 == null && l2 == null) {
-            return new ListNode(0);
-        } else if (l1 == null) {
-            return l2;
-        } else if (l2 == null) {
-            return l1;
-        } else {
-            List<Integer> firstNodeValList = new ArrayList<>();
-            firstNodeValList.add(l1.val);
-            while (l1.next != null) {
-                firstNodeValList.add(l1.next.val);
-                l1 = l1.next;
+        ListNode dummyHead = new ListNode(0);
+        ListNode p = l1, q = l2, curr = dummyHead;
+        int carry = 0;
+        while (p != null || q != null) {
+            int x = (p != null) ? p.val : 0;
+            int y = (q != null) ? q.val : 0;
+            int sum = carry + x + y;
+            carry = sum / 10;
+            curr.next = new ListNode(sum % 10);
+            curr = curr.next;
+            if (p != null) {
+                p = p.next;
             }
-
-            List<Integer> secondNodeValList = new ArrayList<>();
-            secondNodeValList.add(l2.val);
-            while (l2.next != null) {
-                secondNodeValList.add(l2.next.val);
-                l2 = l2.next;
+            if (q != null) {
+                q = q.next;
             }
-
-            int sum = 0;
-            for (int i = 0; i < firstNodeValList.size(); i++) {
-                sum += firstNodeValList.get(i) * 10 ^ i;
-            }
-            for (int i = 0; i < firstNodeValList.size(); i++) {
-                sum += firstNodeValList.get(i) * 10 ^ i;
-            }
-            String[] result = String.valueOf(sum).split("");
-            ListNode root = new ListNode(0);
-            for (int i = 0; i < result.length; i++) {
-
-            }
-
         }
-        return new ListNode(0);
-
+        if (carry > 0) {
+            curr.next = new ListNode(carry);
+        }
+        return dummyHead.next;
     }
 
     public static void main(String[] args) {
@@ -75,11 +113,12 @@ class AddTwoNumbers {
         ListNode x3 = new ListNode(3);
         x1.next = x2;
         x2.next = x3;
-        ListNode y1 = new ListNode(5);
+        ListNode y1 = new ListNode(9);
         ListNode y2 = new ListNode(6);
         ListNode y3 = new ListNode(4);
         y1.next = y2;
         y2.next = y3;
         ListNode re = addTwoNumbers(x1, y1);
+        System.out.println("np");
     }
 }
