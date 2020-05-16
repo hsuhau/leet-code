13e5a50f370c0871f1681c117856f148dba18735
hsuhau <hsuhau@foxmail.com>
2020-05-17 03:10:11 +0800

    搜索插入位置

---
+++ leetcode-easy/src/main/java/com/hsuhau/easy/service/SearchInsertPosition.java
@@ -0,0 +1,50 @@
+package com.hsuhau.easy.service;
+
+// 给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+//
+// 你可以假设数组中无重复元素。
+//
+// 示例 1:
+//
+// 输入: [1,3,5,6], 5
+// 输出: 2
+//
+//
+// 示例 2:
+//
+// 输入: [1,3,5,6], 2
+// 输出: 1
+//
+//
+// 示例 3:
+//
+// 输入: [1,3,5,6], 7
+// 输出: 4
+//
+//
+// 示例 4:
+//
+// 输入: [1,3,5,6], 0
+// 输出: 0
+//
+// Related Topics 数组 二分查找
+
+// leetcode submit region begin(Prohibit modification and deletion)
+public class SearchInsertPosition {
+
+    public int searchInsert(int[] nums, int target) {
+        for (int i = 0; i < nums.length; ++i) {
+            if (nums[i] < target) {
+                if (i + 1 < nums.length && nums[i + 1] > target) {
+                    return i + 1;
+                }
+            } else if (nums[i] == target) {
+                return i;
+            } else {
+                return 0;
+            }
+        }
+        return nums.length;
+    }
+}
+// leetcode submit region end(Prohibit modification and deletion)
---
+++ leetcode-easy/src/test/java/com/hsuhau/easy/service/SearchInsertPositionTest.java
@@ -0,0 +1,16 @@
+package com.hsuhau.easy.service;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SearchInsertPositionTest {
+    SearchInsertPosition searchInsertPosition = new SearchInsertPosition();
+
+    @Test
+    public void testSearchInsert() throws Exception {
+        int result = searchInsertPosition.searchInsert(new int[]{1, 3, 5, 6}, 0);
+        Assert.assertEquals(0, result);
+    }
+}
+
+//Generated with love by TestMe :) Please report issues and submit feature requests at: http://weirddev.com/forum#!/testme
