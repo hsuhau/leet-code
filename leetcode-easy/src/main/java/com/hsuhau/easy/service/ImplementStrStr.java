ac8e7bd2e9dbca56a811e2f417b42feed9441f20
hsuhau <hsuhau@foxmail.com>
2020-05-17 02:48:44 +0800

    实现strStr() update

--- leetcode-easy/src/main/java/com/hsuhau/easy/service/ImplementStrStr.java
+++ leetcode-easy/src/main/java/com/hsuhau/easy/service/ImplementStrStr.java
@@ -1,20 +1,20 @@
 package com.hsuhau.easy.service;
 
-//实现 strStr() 函数。
+// 实现 strStr() 函数。
 //
 // 给定一个 haystack 字符串和一个 needle 字符串，在 haystack 字符串中找出 needle 字符串出现的第一个位置 (从0开始)。如
-//果不存在，则返回 -1。
+// 果不存在，则返回 -1。
 //
 // 示例 1:
 //
 // 输入: haystack = "hello", needle = "ll"
-//输出: 2
+// 输出: 2
 //
 //
 // 示例 2:
 //
 // 输入: haystack = "aaaaa", needle = "bba"
-//输出: -1
+// 输出: -1
 //
 //
 // 说明:
@@ -24,12 +24,71 @@ package com.hsuhau.easy.service;
 // 对于本题而言，当 needle 是空字符串时我们应当返回 0 。这与C语言的 strstr() 以及 Java的 indexOf() 定义相符。
 // Related Topics 双指针 字符串
 
-
-//leetcode submit region begin(Prohibit modification and deletion)
+// leetcode submit region begin(Prohibit modification and deletion)
 public class ImplementStrStr {
-    public int strStr(String haystack, String needle) {
-        return haystack.indexOf(needle);
-    }
-}
-//leetcode submit region end(Prohibit modification and deletion)
+  public int strStr(String haystack, String needle) {
+    return haystack.indexOf(needle);
+  }
 
+  /**
+   * 方法一：子串逐一比较 - 线性时间复杂度 最直接的方法 - 沿着字符换逐步移动滑动窗口，将窗口内的子串与 needle 字符串比较。
+   *
+   * @param haystack
+   * @param needle
+   * @return
+   */
+  public int strStr0(String haystack, String needle) {
+    int L = needle.length(), n = haystack.length();
+
+    for (int start = 0; start < n - L + 1; ++start) {
+      if (haystack.substring(start, start + L).equals(needle)) {
+        return start;
+      }
+    }
+    return -1;
+  }
+
+  /**
+   * 方法二：双指针 - 线性时间复杂度
+   *
+   * @param haystack
+   * @param needle
+   * @return
+   */
+  public int strStr1(String haystack, String needle) {
+
+    //todo 参照官方题解 然而结果不对
+    int L = needle.length(), n = haystack.length();
+
+    if (L == 0) return 0;
+
+    int pn = 0;
+    while (pn < n - L + 1) {
+      while (pn < n - L + 1 && haystack.charAt(pn) != needle.charAt(0)) {
+        pn++;
+      }
+      int pl = 0;
+      while (pn < L && pn < n && haystack.charAt(pn) == needle.charAt(pl)) {
+        ++pn;
+        ++pl;
+      }
+      if (pl == L) {
+        return pn - L;
+      }
+      pn = pn - pl + 1;
+    }
+    return -1;
+  }
+
+  /**
+   * 方法三： Rabin Karp - 常数复杂度
+   * todo
+   * @param haystack
+   * @param needle
+   * @return
+   */
+  public int strStr2(String haystack, String needle) {
+    return -1;
+  }
+}
+// leetcode submit region end(Prohibit modification and deletion)
--- leetcode-easy/src/test/java/com/hsuhau/easy/service/ImplementStrStrTest.java
+++ leetcode-easy/src/test/java/com/hsuhau/easy/service/ImplementStrStrTest.java
@@ -11,6 +11,22 @@ public class ImplementStrStrTest {
         int result = implementStrStr.strStr("haystackneedle", "needle");
         Assert.assertEquals(8, result);
     }
+    @Test
+    public void testStrStr0() throws Exception {
+        int result = implementStrStr.strStr0("haystackneedle", "needle");
+        Assert.assertEquals(8, result);
+    }
+    @Test
+    public void testStrStr1() throws Exception {
+        int result = implementStrStr.strStr1("haystackneedle", "needle");
+        Assert.assertEquals(8, result);
+    }
+    @Test
+    public void testStrStr2() throws Exception {
+        int result = implementStrStr.strStr2("haystackneedle", "needle");
+        Assert.assertEquals(8, result);
+    }
+
 }
 
 //Generated with love by TestMe :) Please report issues and submit feature requests at: http://weirddev.com/forum#!/testme
