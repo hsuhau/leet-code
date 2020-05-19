e99effdd307d0451636a30a59162a9b3c2c30c6e
hsuhau <hsuhau@foxmail.com>
2020-05-19 16:46:38 +0800

    合并两个有序数组，独立实现，实现有问题，nums1并未改变

---
+++ leetcode-easy/src/main/java/com/hsuhau/easy/service/MergeSortedArray.java
@@ -0,0 +1,57 @@
+package com.hsuhau.easy.service;
+
+import com.alibaba.fastjson.JSONObject;
+
+/**
+ * 88. 合并两个有序数组
+ * <p>
+ * 相关标签：数组，双指针
+ * <p>
+ * 给你两个有序整数数组 nums1 和 nums2，请你将 nums2 合并到 nums1 中，使 nums1 成为一个有序数组。
+ * <p>
+ * <p>
+ * <p>
+ * 说明:
+ * <p>
+ * 初始化 nums1 和 nums2 的元素数量分别为 m 和 n 。
+ * 你可以假设 nums1 有足够的空间（空间大小大于或等于 m + n）来保存 nums2 中的元素。
+ * <p>
+ * <p>
+ * 示例:
+ * <p>
+ * 输入:
+ * nums1 = [1,2,3,0,0,0], m = 3
+ * nums2 = [2,5,6],       n = 3
+ * <p>
+ * 输出: [1,2,2,3,5,6]
+ *
+ * @see <a href="https://leetcode-cn.com/problems/merge-sorted-array/">https://leetcode-cn.com/problems/merge-sorted-array/</a>
+ */
+public class MergeSortedArray {
+    /**
+     * @param nums1
+     * @param m
+     * @param nums2
+     * @param n
+     * todo 实现有问题，nums1并未改变
+     */
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int point1 = m - 1;
+        int point2 = n - 1;
+        int point = m + n - 1;
+        int[] res = new int[m + n];
+        System.arraycopy(nums1, 0, res, 0, m);
+        while (point1 >= 0 && point2 >= 0) {
+            if (nums1[point1] >= nums2[point2]) {
+                res[point] = nums1[point1];
+                point1--;
+            } else {
+                res[point] = nums2[point2];
+                point2--;
+            }
+            point--;
+        }
+
+        System.out.println("nums1 = " + JSONObject.toJSON(nums1));
+    }
+}
---
+++ leetcode-easy/src/test/java/com/hsuhau/easy/service/MergeSortedArrayTest.java
@@ -0,0 +1,20 @@
+package com.hsuhau.easy.service;
+
+import com.alibaba.fastjson.JSONObject;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MergeSortedArrayTest {
+    MergeSortedArray mergeSortedArray = new MergeSortedArray();
+
+    @Test
+    public void testMerge() throws Exception {
+        int[] nums1 = new int[]{1, 2, 3};
+        int[] nums2 = new int[]{2, 5, 6};
+        mergeSortedArray.merge(nums1, 3, nums2, 3);
+        System.out.println(JSONObject.toJSON(nums1));
+        Assert.assertEquals(nums1, new int[]{1, 2, 2, 3, 5, 6});
+    }
+}
+
+//Generated with love by TestMe :) Please report issues and submit feature requests at: http://weirddev.com/forum#!/testme
