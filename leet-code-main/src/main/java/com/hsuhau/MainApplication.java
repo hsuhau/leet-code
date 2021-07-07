7d74e855acb8e33ecde6219ca8c343df9e7a565b
hsuhau <hsuhau@foxmail.com>
2021-07-07 13:59:27 +0800

    update configuration

--- leet-code-main/src/main/java/com/hsuhau/MainApplication.java
+++ leet-code-main/src/main/java/com/hsuhau/MainApplication.java
@@ -3,6 +3,9 @@ package com.hsuhau;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author hsuhau
+ */
 @SpringBootApplication
 public class MainApplication {
     public static void main(String[] args) {
