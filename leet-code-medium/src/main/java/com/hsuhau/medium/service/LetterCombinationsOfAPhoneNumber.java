001187de615235d318b2f98dbdb92974ac453c36
hsuhau <36336469+hsuhau@users.noreply.github.com>
2021-12-17 16:26:30 +0800

    Update LetterCombinationsOfAPhoneNumber.java

--- leet-code-medium/src/main/java/com/hsuhau/medium/service/LetterCombinationsOfAPhoneNumber.java
+++ leet-code-medium/src/main/java/com/hsuhau/medium/service/LetterCombinationsOfAPhoneNumber.java
@@ -1,41 +1,43 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+package com.hsuhau.medium.service;
+
+import org.springframework.stereotype.Service;
+
+import java.util.*;
 
 /**
  * 17. 电话号码的字母组合
  * 给定一个仅包含数字 2-9 的字符串，返回所有它能表示的字母组合。答案可以按 任意顺序 返回。
- * 
+ * <p>
  * 给出数字到字母的映射如下（与电话按键相同）。注意 1 不对应任何字母。
- * 
- * 
- * 
- * 
- * 
+ * <p>
+ * <p>
+ * <p>
+ * <p>
+ * <p>
  * 示例 1：
- * 
+ * <p>
  * 输入：digits = "23"
  * 输出：["ad","ae","af","bd","be","bf","cd","ce","cf"]
  * 示例 2：
- * 
+ * <p>
  * 输入：digits = ""
  * 输出：[]
  * 示例 3：
- * 
+ * <p>
  * 输入：digits = "2"
  * 输出：["a","b","c"]
- * 
- * 
+ * <p>
+ * <p>
  * 提示：
- * 
+ * <p>
  * 0 <= digits.length <= 4
  * digits[i] 是范围 ["2", "9"] 的一个数字。
  */
+@Service
 public class LetterCombinationsOfAPhoneNumber {
+
     public List<String> letterCombinations(String digits) {
-        Map<Integer, List<String>> letters = new HashMap<>() {
+        Map<Integer, List<String>> letters = new HashMap<Integer, List<String>>() {
             {
                 put(2, Arrays.asList("a", "b", "c"));
                 put(3, Arrays.asList("d", "e", "f"));
