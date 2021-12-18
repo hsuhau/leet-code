cbeeb1b8636fa0e35fb4ebd6df4323e35c22b181
hsuhau <36336469+hsuhau@users.noreply.github.com>
2021-12-18 16:56:56 +0800

    Update LetterCombinationsOfAPhoneNumberTest.java

--- leet-code-medium/src/test/java/com/hsuhau/medium/service/LetterCombinationsOfAPhoneNumberTest.java
+++ leet-code-medium/src/test/java/com/hsuhau/medium/service/LetterCombinationsOfAPhoneNumberTest.java
@@ -1,26 +1,33 @@
 package com.hsuhau.medium.service;
 
-import java.util.List;
-
 import com.hsuhau.MainApplication;
-
-import org.junit.Assert;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.util.List;
+
+/**
+ * junit 5 test case
+ */
 @SpringBootTest(classes = MainApplication.class)
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
+@DisplayName("17.电话号码的字母组合-测试用例")
 public class LetterCombinationsOfAPhoneNumberTest {
     @Autowired
     private LetterCombinationsOfAPhoneNumber letterCombinationsOfAPhoneNumber;
 
-    @Test
-    public void testLetterCombinations() {
-        String digits = "2367";
+    @DisplayName("2367")
+    @ParameterizedTest
+    @ValueSource(strings = {"2367"})
+    public void testLetterCombinations(String digits) {
         List<String> result = letterCombinationsOfAPhoneNumber.letterCombinations(digits);
-        Assert.assertEquals(result.get(result.size() - 1), "cfos");
+        Assertions.assertEquals("cfos", result.get(result.size() - 1));
     }
 }
