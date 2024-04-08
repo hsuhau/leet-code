85e48fa5de6edeb774d2752f96391133133237c1
hsuhau <hsuhau@foxmail.com>
2024-04-08 10:38:01 +0800

    70.爬楼梯-测试用例

--- leet-code-easy/src/test/java/com/hsuhau/easy/service/ClimbingStairsTest.java
+++ leet-code-easy/src/test/java/com/hsuhau/easy/service/ClimbingStairsTest.java
@@ -3,6 +3,7 @@ package com.hsuhau.easy.service;
 import com.hsuhau.MainApplication;
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,6 +11,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootTest(classes = MainApplication.class)
 @RunWith(SpringRunner.class)
+@DisplayName("70.爬楼梯-测试用例")
 public class ClimbingStairsTest {
 
     @Autowired
@@ -17,7 +19,7 @@ public class ClimbingStairsTest {
 
     @Test
     public void hammingWeight() {
-        int result = climbingStairs.hammingWeight(3);
-        Assert.assertEquals(3, result);
+        int result = climbingStairs.climbStair2(4);
+        Assert.assertEquals(5, result);
     }
 }
